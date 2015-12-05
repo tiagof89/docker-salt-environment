dff0901bc148217eeeb0b1487009fd5b52fa77b3
Dawid Malinowski <dawidmalina@gmail.com>
2015-12-05 23:56:48 +0100

    Add master configuration

---
+++ master/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Variables from environement
+: "${LOG_LEVEL:=info}"
+: "${OPTIONS:=}"
+
+# Salt master also start minion in background
+echo "INFO: Starting salt-minion and auto connect to salt-master"
+service salt-minion start
+
+# Start salt-master
+echo "INFO: Starting salt-master with log level $LOG_LEVEL with hostname master"
+/usr/bin/salt-master --log-level=$LOG_LEVEL $OPTIONS
