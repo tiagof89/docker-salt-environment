e78daf88fb44ba28b31ad907ae40dad8d4411d46
Dawid Malinowski <d.malinowski@oberthur.com>
2016-09-25 23:36:32 +0200

    Add salt-api

--- master/start.sh
+++ master/start.sh
@@ -8,6 +8,9 @@
 echo "INFO: Starting salt-minion and auto connect to salt-master"
 service salt-minion start
 
+# start salt-api
+/etc/init.d/salt-api start
+
 # Start salt-master
 echo "INFO: Starting salt-master with log level $LOG_LEVEL with hostname master"
 /usr/bin/salt-master --log-level=$LOG_LEVEL $OPTIONS
